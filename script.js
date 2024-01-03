aae925978f4417b7078b7488b6b5d6decfebea86
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-03 23:17:07 +0000

    modified scripts to populate the value of user inputs

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
 let inputForm = $('#input-form');
+
 const inputFormBtn = $('#btn');
 const nameStore = [];
+console.log(inputFormBtn);
 
 
 inputFormBtn.on("click", function (){
     let inputFormValue = inputForm.val();
     nameStore.push(inputFormValue);
     console.log(nameStore);
-   
-    // $('ol').append("<li style='margin-left: 25px; font-size: 30px;'>" + inputFormValue + 
-    // "<table><tr><td><td> <td></td> </tr></table><span style='margin-left: 250px;'><button id='btn'> Update </button><span> <span style='margin-left: 30px;'><button id='btn' onclick='toDelete()' class='btn-class'> Delete </button><span></li><br>");
- 
-    // $('ol').append("<span style='display: inline;'><li style='margin-left: 25px; font-size: 30px;'>" + inputFormValue + " <table><tr><td><button id='btn'> Update </button></td><td><button id='btn' onclick='toDelete()' class='btn-class'> Delete </button></td></tr></table></span></li>");
-    $('ol').append("<span style='display: inline;'><li style='margin-left: 25px; font-size: 30px;'>" + inputFormValue + " <button id='btn'> Update </button><button id='btn' onclick='toDelete()' class='btn-class'> Delete </button></span></li>");
+    $('#workers-data-list').append("<div style='display:flex; justify-content:space-between;'><span>"+inputFormValue+"</span><span><button>Update</button><button>Delete</button></span></div>");
 
-
-
-
-    // <span style='margin-left: 250px;'><span> <span style='margin-left: 30px;'><span></li><br>
-    // <button> Update </button><button> Delete </button>
 });
 
-// function toDelete(){
-//     let btnClass = document.querySelector(".btn-class");
-//     btnClass.addEventListener('click',function(){
-//         this.pop();
-//     });
-
-// }
 
 
 
