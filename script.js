17b0dae97adddc2c141d5a428b2d52d4ac59f43e
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-04 00:35:06 +0000

    added <br> to Jquery DOM manipulation

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ inputFormBtn.on("click", function (){
     let inputFormValue = inputForm.val();
     nameStore.push(inputFormValue);
     console.log(nameStore);
-    $('#workers-data-list').append("<div style='display:flex; justify-content:space-between;'><span>"+inputFormValue+"</span><span><button  class='btn-update' style='margin-right:10px;'>Update</button><button class='btn-delete'>Delete</button></span></div>");
+    $('#workers-data-list').append("<div style='display:flex; justify-content:space-between;'><span>"+inputFormValue+"</span><span><button  class='btn-update' style='margin-right:10px;'>Update</button><button class='btn-delete'>Delete</button></span></div><br>");
 
 });
 
