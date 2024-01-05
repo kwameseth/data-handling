1d243d9a73c15a323fec1196ce60c355e2e46e43
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-05 03:03:57 +0000

    few code snippets to optimize pafe behavior #5

--- script.js
+++ script.js
@@ -17,13 +17,15 @@ inputFormBtn.on("click", function (){
         console.log(nameStore);
         $('#workers-data-list').append("<div style='display:flex; justify-content:space-between;'><span>"+inputFormValue+"</span><span><button  class='btn-update' style='margin-right:10px;'>Update</button><button class='btn-delete'>Delete</button></span></div><br>");
     } else {
-        $('#workers-data-list').append("<div class='visi' style='display:flex; justify-content:space-between;'><span>You entered nothing!!</span><span id='btn-clear'><button  class='btn-clear' >Clear</button></span></div><br>");      
+        $('#workers-data-list').append("<div class='visi' style='display:flex; justify-content:space-between;'><span>You entered nothing!!</span><span id='btn-clear'><button  class='btn-clear' onclick='btnClear()' >Clear</button></span></div><br>");      
 }
 });
 
-$('button.btn-clear').on("click", function () { 
-    $('.visi').css("display", "none");
+function btnClear () {
+$('#workers-data-list .visi #btn-clear').click(function () { 
+    alert("about to clear you!");
 });
+}
 
 
 
