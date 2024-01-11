e5e25db3a9fa6e18bdc961aed747af4845e812a2
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-11 19:10:40 +0000

    added functionality to delete btn

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let nameStore = [];
 const inputForm = document.getElementById('input-form');
 const inputFormBtn = document.getElementById('btn');
 const divCount = document.querySelector('#divCount');
+
+
 divCount.style.color = 'green';
 inputForm.style.color = '#3666f5';
 const maxCharacter = 15;
@@ -13,7 +15,6 @@ const showDetailsSection = document.getElementById('workers-data-list');
 inputForm.addEventListener('keyup', function(){
     const showCounterOfText = maxCharacter - this.value.length;
     divCount.innerText = "Characters left: " + showCounterOfText;
-    console.log(showCounterOfText);
 
     if(showCounterOfText < 0){
         styleFormAndBtn('#d3d3d3', true,'#d3d3d3', '#d3d3d3', 'none');
@@ -44,10 +45,18 @@ function styleFormAndBtn(textCountColor, disabedBool, backGroundColor, textColor
 
 }
 
+
+
 function saveForm(){
-    let inputFormInfo = inputForm.value;
-    nameStore.push(inputFormInfo);
+    const id = '' + new Date().getTime();
+    const inputFormInfo = inputForm.value;
+    
+    nameStore.push({
+        SN: id,
+        dataName: inputFormInfo
+    });
     console.log(nameStore);
+
     renderDetails();
 }
 
@@ -55,23 +64,64 @@ function renderDetails() {
     document.getElementById('workers-data-list').innerHTML = '';
 
     nameStore.forEach(function (nameSaved) {
-        if(nameSaved == ""){
-            renderDynamics('Nothing entered', 'clear');
+        if(nameSaved.dataName == ""){
+            renderDynamics('Nothing entered', 'clear', nameSaved.SN);
+
         } else {
-           renderDynamics(nameSaved, 'delete');
+           renderDynamics(nameSaved.dataName, 'delete', nameSaved.SN);
         }
     });   
 }
 
-function renderDynamics (storedData, btnName){
+function renderDynamics (storedData, btnName, btnID){
     const newDiv = document.createElement('div');
     newDiv.innerText = storedData;
-    const newBtn = document.createElement('button');
-    newBtn.innerText = btnName;
-    newBtn.style.marginLeft = '30px';
+    newDiv.style.marginTop = '10px';
+
+    if(btnName == 'clear'){
+        buttonRenderFeatures('#3666f5', btnName, btnID);
+    }
+    else {
+        buttonRenderFeatures('red', btnName, btnID);
+    }
     newDiv.appendChild(newBtn);
     showDetailsSection.appendChild(newDiv);
 }
+
+function buttonRenderFeatures(backGroundColor, btnInfo, id){
+    newBtn = document.createElement('button');
+    newBtn.innerText = btnInfo;
+    newBtn.id = id;
+    newBtn.style.marginLeft = '30px';
+    newBtn.style.backgroundColor = backGroundColor;
+    newBtn.style.color = '#fff';
+    newBtn.style.border = 'none';
+    newBtn.style.padding = '5px 20px';
+    newBtn.style.borderRadius = '4px';
+    newBtn.style.cursor = 'pointer';
+    newBtn.style.fontSize = '13px';
+
+    newBtn.onclick = btnDeleteOrClear;
+    
+}
+
+function btnDeleteOrClear (e){
+    const btnID = e.target //to get butoon details
+    const btnIDNumber = btnID.id;
+
+
+    nameStore = nameStore.filter((names) => {
+        if(names.SN === btnIDNumber){
+            return false;
+        } else {
+            return true;
+        }
+    });
+    renderDetails();
+    console.log(nameStore);
+}
+
+
 
 
 
