2f8640db4e47ab53946dc6d24856de91caeb6006
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-11 11:28:15 +0000

    added the rendering structure

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const divCount = document.querySelector('#divCount');
 divCount.style.color = 'green';
 inputForm.style.color = '#3666f5';
 const maxCharacter = 15;
-// inputForm.maxLength = maxCharacter;
+const showDetailsSection = document.getElementById('workers-data-list');
+
 
 inputForm.addEventListener('keyup', function(){
     const showCounterOfText = maxCharacter - this.value.length;
@@ -15,15 +16,15 @@ inputForm.addEventListener('keyup', function(){
     console.log(showCounterOfText);
 
     if(showCounterOfText < 0){
-        styleFormAndBtn('#d3d3d3', true,'#d3d3d3', '#3666f5');
+        styleFormAndBtn('#d3d3d3', true,'#d3d3d3', '#d3d3d3', 'none');
     }
 
     else if(showCounterOfText <= 3){
-        styleFormAndBtn('red', false,'#3666f5', '#3666f5');
+        styleFormAndBtn('red', false,'#3666f5', '#3666f5', '2px solid #B4BDFF');
     }
 
     else if(showCounterOfText <= 6){
-        styleFormAndBtn('#977316', false,'#3666f5', '#3666f5');
+        styleFormAndBtn('#977316', false,'#3666f5', '#3666f5', '2px solid #B4BDFF');
     }
 
     else {
@@ -34,11 +35,12 @@ inputForm.addEventListener('keyup', function(){
  
 });
 
-function styleFormAndBtn(textCountColor, disabedBool, backGroundColor, textColor){
+function styleFormAndBtn(textCountColor, disabedBool, backGroundColor, textColor, borderStatus){
     divCount.style.color = textCountColor;
     inputFormBtn.disabled = disabedBool;
     inputFormBtn.style.backgroundColor = backGroundColor;
     inputForm.style.color = textColor;
+    inputForm.style.border = borderStatus;
 
 }
 
@@ -46,6 +48,29 @@ function saveForm(){
     let inputFormInfo = inputForm.value;
     nameStore.push(inputFormInfo);
     console.log(nameStore);
+    renderDetails();
+}
+
+function renderDetails() {
+    document.getElementById('workers-data-list').innerHTML = '';
+
+    nameStore.forEach(function (nameSaved) {
+        if(nameSaved == ""){
+            renderDynamics('Nothing entered', 'clear');
+        } else {
+           renderDynamics(nameSaved, 'delete');
+        }
+    });   
+}
+
+function renderDynamics (storedData, btnName){
+    const newDiv = document.createElement('div');
+    newDiv.innerText = storedData;
+    const newBtn = document.createElement('button');
+    newBtn.innerText = btnName;
+    newBtn.style.marginLeft = '30px';
+    newDiv.appendChild(newBtn);
+    showDetailsSection.appendChild(newDiv);
 }
 
 
